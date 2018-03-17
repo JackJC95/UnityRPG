b7dca0c522aef671c6916021b2c58f1eac690891
JackJC95 <jj-mousey@hotmail.co.uk>
2018-03-17 16:21:19 +0000

    L6 DIY follow camera (initial commit)

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position;
+    }
+}
