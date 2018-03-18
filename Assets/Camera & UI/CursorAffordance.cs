e3e84f5a1acd7b989bc5a4de59c83306fcf13f94
JackJC95 <jj-mousey@hotmail.co.uk>
2018-03-18 14:36:55 +0000

    L20 Various bug fixes

--- Assets/Camera & UI/CursorAffordance.cs
+++ Assets/Camera & UI/CursorAffordance.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent (typeof(CameraRaycaster))]
 public class CursorAffordance : MonoBehaviour
 {
     [SerializeField] Texture2D walkCursor = null;
     [SerializeField] Texture2D unknownCursor = null;
     [SerializeField] Texture2D targetCursor = null;
-    [SerializeField] Vector2 cursorHotspot = new Vector2(96, 96);
+    [SerializeField] Vector2 cursorHotspot = new Vector2(0, 0);
 
     CameraRaycaster cameraRaycaster;
 
@@ -18,7 +20,7 @@ public class CursorAffordance : MonoBehaviour
 	
 	void LateUpdate ()
     {
-        switch (cameraRaycaster.layerHit)
+        switch (cameraRaycaster.currentLayerHit)
         {
             case Layer.Walkable:
                 Cursor.SetCursor(walkCursor, cursorHotspot, CursorMode.Auto);
