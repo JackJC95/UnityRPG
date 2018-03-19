4740981b9d8ed29357c81bfa892b5d526f641cf4
JackJC95 <jj-mousey@hotmail.co.uk>
2018-03-19 00:11:14 +0000

    L43 Windmill rotating

--- Assets/Camera & UI/CameraRaycaster.cs
+++ Assets/Camera & UI/CameraRaycaster.cs
@@ -52,6 +52,7 @@ public class CameraRaycaster : MonoBehaviour
         // Otherwise return background hit
         raycastHit.distance = distanceToBackground;
         layerHit = Layer.RaycastEndStop;
+        layerChangeObservers(layerHit);
     }
 
     RaycastHit? RaycastForLayer(Layer layer)
---
+++ Assets/Utility/SpinMe.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinMe : MonoBehaviour
+{
+
+	[SerializeField] float xRotationsPerMinute = 1f;
+	[SerializeField] float yRotationsPerMinute = 1f;
+	[SerializeField] float zRotationsPerMinute = 1f;
+	
+	void Update ()
+    {
+        float xDegreesPerFrame = (Time.deltaTime / 60) * xRotationsPerMinute * 360; // TODO COMPLETE ME
+        transform.RotateAround (transform.position, transform.right, xDegreesPerFrame);
+
+		float yDegreesPerFrame = (Time.deltaTime / 60) * yRotationsPerMinute * 360; // TODO COMPLETE ME
+        transform.RotateAround (transform.position, transform.up, yDegreesPerFrame);
+
+        float zDegreesPerFrame = (Time.deltaTime / 60) * zRotationsPerMinute * 360; // TODO COMPLETE ME
+        transform.RotateAround (transform.position, transform.forward, zDegreesPerFrame);
+	}
+}
