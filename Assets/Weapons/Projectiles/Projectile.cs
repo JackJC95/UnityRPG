1a6c3a564f27dc95f4809c428fb4b506104ee24b
JackJC95 <jj-mousey@hotmail.co.uk>
2018-03-23 20:55:29 +0000

    L84 Projectiles discriminate (fixed movement)

--- Assets/Weapons/Projectiles/Projectile.cs
+++ Assets/Weapons/Projectiles/Projectile.cs
@@ -3,22 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
-{
+{    
+    [SerializeField] float projectileSpeed;
+    [SerializeField] GameObject shooter; // so can inspect when paused
+
+    const float DESTROY_DELAY = 0.01f;
     float damageCaused;
-    public float projectileSpeed;
+
+    public void SetShooter(GameObject shooter)
+    {
+        this.shooter = shooter;
+    }
 
     public void SetDamage(float damage)
     {
         damageCaused = damage;
     }
 
+    public float GetDefaultLaunchSpeed()
+    {
+        return projectileSpeed;
+    }
+
     private void OnCollisionEnter(Collision collision)
+    {
+        var layerCollidedWith = collision.gameObject.layer;
+        if (layerCollidedWith != shooter.layer)
+        {
+            DamageIfDamageable(collision);
+        }
+    }
+
+    private void DamageIfDamageable(Collision collision)
     {
         Component damageableComponent = collision.gameObject.GetComponent(typeof(IDamageable));
         if (damageableComponent)
         {
             (damageableComponent as IDamageable).TakeDamage(damageCaused);
         }
-        Destroy(gameObject, 0.01f);
+        Destroy(gameObject, DESTROY_DELAY);
     }
 }
