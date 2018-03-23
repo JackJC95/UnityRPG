59842ccaee13e946ca8d7da42a62bb0aabfd6be9
JackJC95 <jj-mousey@hotmail.co.uk>
2018-03-23 00:51:37 +0000

    L83 Animation pack imported (MOVEMENT BROKEN)

--- Assets/Characters/Player/Player.cs
+++ Assets/Characters/Player/Player.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class Player : MonoBehaviour, IDamageable
 {
@@ -10,8 +12,9 @@ public class Player : MonoBehaviour, IDamageable
     [SerializeField] float minTimeBetweenHits = 0.5f;
     [SerializeField] float maxAttackRange = 2f;
 
+    [SerializeField] Weapon weaponInUse;
+
     CameraRaycaster cameraRaycaster;
-    GameObject currentTarget;
     float currentHealthPoints;
     float lastHitTime = 0f;
 
@@ -25,9 +28,33 @@ public class Player : MonoBehaviour, IDamageable
 
     private void Start()
     {
+        RegisterForMouseClick();
+        currentHealthPoints = maxHealthPoints;
+        PutWeaponInHand();
+    }
+
+    private void PutWeaponInHand()
+    {
+        var weaponPrefab = weaponInUse.GetWeaponPrefab();
+        GameObject dominantHand = RequestDominantHand();
+        var weapon = Instantiate(weaponPrefab, dominantHand.transform);
+        weapon.transform.localPosition = weaponInUse.gripTransform.localPosition;
+        weapon.transform.localRotation = weaponInUse.gripTransform.localRotation;
+    }
+
+    private GameObject RequestDominantHand()
+    {
+        var dominantHands = GetComponentsInChildren<DominantHand>();
+        int numberOfDominantHands = dominantHands.Length;
+        Assert.AreNotEqual(numberOfDominantHands, 0, "No dominant hand found on player, please add one");
+        Assert.IsFalse(numberOfDominantHands > 1, "Multiple dominant hand scripts on player, please remove one");
+        return dominantHands[0].gameObject;
+    }
+
+    private void RegisterForMouseClick()
+    {
         cameraRaycaster = FindObjectOfType<CameraRaycaster>();
         cameraRaycaster.notifyMouseClickObservers += OnMouseClicked;
-        currentHealthPoints = maxHealthPoints;
     }
 
     void OnMouseClicked(RaycastHit raycastHit, int layerHit)
@@ -42,7 +69,6 @@ public class Player : MonoBehaviour, IDamageable
                 return;
             }
 
-            currentTarget = enemy;
             var enemyComponent = enemy.GetComponent<Enemy>();
             if (Time.time - lastHitTime > minTimeBetweenHits)
             {
---
+++ Assets/Weapons/Weapon.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = ("RPG/Weapon"))]
+public class Weapon : ScriptableObject
+{
+    public Transform gripTransform;
+
+    [SerializeField] GameObject weaponPrefab;
+    [SerializeField] AnimationClip attackAnimation;
+
+    public GameObject GetWeaponPrefab()
+    {
+        return weaponPrefab;
+    }
+}
