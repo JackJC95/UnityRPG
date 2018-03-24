af853b01bf16b61fbcde0da351ea73d40a39678a
JackJC95 <jj-mousey@hotmail.co.uk>
2018-03-24 20:10:29 +0000

    L111 Special ability template set up

---
+++ Assets/Characters/Special Abilities/Power Attack/PowerAttackConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Characters
+{
+    [CreateAssetMenu(menuName = ("RPG/Special Ability/Power Attack"))]
+    public class PowerAttackConfig : SpecialAbilityConfig
+    {
+        [Header("Special Ability General")]
+        [SerializeField] float extraDamage = 10f;
+
+        public override ISpecialAbility AddComponent(GameObject gameObjectToAttachTo)
+        {
+            var behaviourComponent = gameObjectToAttachTo.AddComponent<PowerAttackBehaviour>();
+            behaviourComponent.SetConfig(this);
+            return behaviourComponent;
+        }
+    }
+}
---
+++ Assets/Characters/Special Abilities/SpecialAbilityConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Characters
+{
+    public abstract class SpecialAbilityConfig : ScriptableObject
+    {
+        [Header("Special Ability General")]
+        [SerializeField] float energyCost = 10f;
+
+        abstract public ISpecialAbility AddComponent(GameObject gameObjectToAttachTo);
+    }
+}
